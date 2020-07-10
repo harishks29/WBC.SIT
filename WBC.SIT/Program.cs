a2de6f0e9e3cb83ed8a0acd54f31e4496aa5fd7a
Harish <harish.ks29@gmail.com>
2020-07-10 19:48:56 +0530

    First commit

---
+++ WBC.SIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WBC.SIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
